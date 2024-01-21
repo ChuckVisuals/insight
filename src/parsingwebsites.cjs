19619992ae3c6da2f32c029a95c9cb70f941fa4b
gloryndu <endu@terpmail.umd.edu>
2024-01-20 22:21:53 -0800

    teet

--- src/parsingwebsites.cjs
+++ src/parsingwebsites.cjs
@@ -1,26 +1,42 @@
-import axios from 'axios';
-const axios = require('axios');
+// import axios from 'axios';
+// const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://gymshark.p.rapidapi.com/2022-12/gymshark/latestproducts',
-  headers: {
-    'X-RapidAPI-Key': '56498b123bmsh2e0f22b0320239ep14cce8jsn3fe658da82b7',
-    'X-RapidAPI-Host': 'gymshark.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://gymshark.p.rapidapi.com/2022-12/gymshark/latestproducts',
+//   headers: {
+//     'X-RapidAPI-Key': '56498b123bmsh2e0f22b0320239ep14cce8jsn3fe658da82b7',
+//     'X-RapidAPI-Host': 'gymshark.p.rapidapi.com'
+//   }
+// };
 
-async function main() {
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+// async function main() {
+//   try {
+//     const response = await axios.request(options);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// main();
+cosnt puppeteer = require('puppeteer')
+async function scrapeProduct(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage()
+    await page.goto(url)
+    const[el] = await page.$x('//*[@id="MainContent"]/div[1]/section[1]/div[1]')
+    const src = await el.getProperty('src');
+    const srcTxt = await src.jsonValue();
+
+    console.log({srcTxt});
+
+    browser.close()
+
+
 }
 
-main();
-
+scrapeProduct('https://www.gymshark.com/?gclsrc=aw.ds&gad_source=1&gclid=Cj0KCQiA-62tBhDSARIsAO7twbaOeAZxGwPccigRplSgyI5Cn4Lb2rpmXBowXQrPyFcAZC0fkDDH9x4aAsvCEALw_wcB&gclsrc=aw.ds')
 // const axios = require('axios');
 // const cheerio = require('cheerio');
 
